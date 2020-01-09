8f86cd78f6e29301ae26fcb070fc25164b3b8a27
Eder Correia Lira <madskull@gmail.com>
2020-01-08 22:44:45 -0300

    Finalização exercicio 10

--- 10 - OpeMatematica/OpeMatematica.dpr
+++ 10 - OpeMatematica/OpeMatematica.dpr
@@ -6,19 +6,59 @@ program OpeMatematica;
 
 uses
   System.SysUtils,
+  System.Math,
   uClasseMatematica in 'uClasseMatematica.pas';
 
 var
   oClasseMatematica: TClasseMatematica;
-  nResultado: double;
+  sOpcao: string;
+
+function OpcaoEhValida(const pValor: string): boolean;
+begin
+  result := (StrToIntDef(pValor, ZeroValue) > ZeroValue);
+  if not result then
+  begin
+    writeln('Informe uma opção válida.');
+    writeln(EmptyStr);
+  end;
+end;
+
+function LerValor(const pDescricao: string): double;
+var
+  sValor: string;
+  nValor: double;
+begin
+  repeat
+    Write(pDescricao + ': ');
+    Readln(sValor);
+    nValor := StrToFloatDef(sValor, ZeroValue);
+  until nValor <> ZeroValue;
+  result := nValor;
+end;
 
 begin
+  ReportMemoryLeaksOnShutdown := True;
+
+  repeat
+    writeln('1 - Soma');
+    writeln('2 - Subtração');
+    writeln('3 - Multiplicação');
+    writeln('4 - Divisão');
+    write('Informe a operação desejada: ');
+    readln(sOpcao);
+  until OpcaoEhValida(sOpcao);
+
   oClasseMatematica := TClasseMatematica.Create;
   try
-    oClasseMatematica.Esquerda := 2;
-    oClasseMatematica.Direita := 2;
-    oClasseMatematica.OperacaoMatematica := function: double begin result := oClasseMatematica.Esquerda * oClasseMatematica.Direita end;
-    Writeln(oClasseMatematica.OperacaoMatematica.ToString);
+    oClasseMatematica.Esquerda := LerValor('Esquerda');
+    oClasseMatematica.Direita := LerValor('Direita');
+    case sOpcao.ToInteger of
+      1 : oClasseMatematica.OperacaoMatematica := function: double begin result := oClasseMatematica.Esquerda + oClasseMatematica.Direita end;
+      2 : oClasseMatematica.OperacaoMatematica := function: double begin result := oClasseMatematica.Esquerda - oClasseMatematica.Direita end;
+      3 : oClasseMatematica.OperacaoMatematica := function: double begin result := oClasseMatematica.Esquerda * oClasseMatematica.Direita end;
+      4 : oClasseMatematica.OperacaoMatematica := function: double begin result := oClasseMatematica.Esquerda / oClasseMatematica.Direita end;
+    end;
+    Writeln('Resultado: ' + oClasseMatematica.OperacaoMatematica.ToString);
     readln;
   finally
     FreeAndNil(oClasseMatematica);
