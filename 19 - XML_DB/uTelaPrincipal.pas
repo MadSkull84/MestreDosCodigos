8b16b8c4d2b91ff987ed66cfe64b5189452e3f95
Eder Correia Lira <madskull@gmail.com>
2020-01-20 18:27:40 -0300

    Adicionado cabeçalho exe 19 - XML DB

--- 19 - XML_DB/uTelaPrincipal.pas
+++ 19 - XML_DB/uTelaPrincipal.pas
@@ -1,3 +1,27 @@
+{
+19 - Faça o download do XML. Crie um projeto VCL no Delphi.
+     Utilizando o assistante XML Data Binding, faça a leitura do XML previamente
+     baixado. Leia o conteúdo do XML nas classes geradas e o apresente em um
+     componente TMemo no seguinte formato:
+
+    ID: 3
+    Nome: Tanyahbbhvvg
+    Sobrenome: Koch
+    Gênero: female
+    Data de Nascimento: 22/09/1976
+    E-mail: clangosh@example.com
+    Telefone: 1-440-621-2581 x967
+    Website: http://www.halvorson.com/rerum-soluta-dolores-delectus-inventore-rem-architecto
+    Endereço: 812 Terry Wall Alvahchester, ID 41791-2432
+    Status: active
+    Link (HATEOAS):
+    |_ Recurso: https://gorest.co.in/public-api/users/3
+    |_ Editar: https://gorest.co.in/public-api/users/3
+    |_ Avatar: https://lorempixel.com/250/250/people/?23168
+
+Autor: Eder Correia Lira
+}
+
 unit uTelaPrincipal;
 
 interface
